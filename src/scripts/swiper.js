97a0a2e442e3fa1d81bd18a92462ed51cbbb2922
romawakeup <romawakeup@mail.ru>
2024-11-01 12:03:12 +0300

    update

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -1,36 +1,107 @@
 const swiperNews = new Swiper(".news-swiper", {
   loop: true,
-  autoplay: {
-    delay: 3000,
-  },
-  slidesPerView: 4,
+  slidesPerView: "auto",
   spaceBetween: 35,
   freeMode: true,
+  // breakpoints: {
+  //   20: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 15,
+  //     slideToClickedSlide: true,
+  //   },
 
+  //   1000: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 20,
+  //     slideToClickedSlide: true,
+  // },
+  //   1300: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 35,
+  //     slideToClickedSlide: true,
+  // },
+  // },
+  navigation: {
+    nextEl: ".btn-next-news",
+    prevEl: ".btn-prev-news",
+  },
 });
 
 const swiperPlaces = new Swiper(".places-swiper", {
   loop: true,
-  // autoplay: {
-  //   delay: 3000,
-  // },
-  slidesPerView: 4.5,
+  slidesPerView: 4,
   spaceBetween: 20,
+  navigation: {
+    nextEl: ".btn-next-places",
+    prevEl: ".btn-prev-places",
+  },
+  breakpoints: {
+    20: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+      slideToClickedSlide: true,
+    },
+
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+      slideToClickedSlide: true,
+  },
+    1300: {
+      slidesPerView: 4,
+      spaceBetween: 35,
+      slideToClickedSlide: true,
+  },
+  },
 });
 
 const swiperExc = new Swiper(".excursions-swiper", {
   loop: true,
-  // autoplay: {
-  //   delay: 3000,
-  // },
   slidesPerView: 4.5,
   spaceBetween: 20,
+  navigation: {
+    nextEl: ".btn-next-exc",
+    prevEl: ".btn-prev-exc",
+  },
+  breakpoints: {
+    20: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+      slideToClickedSlide: true,
+    },
+
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+      slideToClickedSlide: true,
+  },
+    1300: {
+      slidesPerView: 4,
+      spaceBetween: 35,
+      slideToClickedSlide: true,
+  },
+  },
 });
 
 const swiperEvents = new Swiper(".events-swiper", {
   loop: true,
-  // autoplay: {
-  //   delay: 3000,
-  // },
   slidesPerView: 4.5,
+  breakpoints: {
+    20: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+      slideToClickedSlide: true,
+    },
+
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+      slideToClickedSlide: true,
+  },
+    1300: {
+      slidesPerView: 4,
+      spaceBetween: 35,
+      slideToClickedSlide: true,
+  },
+  },
 });
